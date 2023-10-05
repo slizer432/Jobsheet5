dc0dafbc46a98d2a1b6be861cd2db873e2aea01e
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-10-05 13:53:51 +0700

    mengembalikan data hilang

---
+++ PemilihanPercobaan123.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PemilihanPercobaan123 {
+    public static void main(String[] args) {
+        Scanner input23 = new Scanner(System.in);
+        System.out.println("Masukkan angka: ");
+        int angka = input23.nextInt();
+        String hasil = (angka % 2 == 0) ? "Angka "+angka+" bilangan genap" : "angka "+" bilangan ganjil";
+        System.out.println(hasil);
+
+    }
+
+}
